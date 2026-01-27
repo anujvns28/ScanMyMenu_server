f568aac385d28e4ac350c3223611eb945251fc4d
anujYadav <anujvns28@gmail.com>
2026-01-27 18:31:49 +0530

    callback url fixed

--- config/googleConfig.js
+++ config/googleConfig.js
@@ -2,6 +2,8 @@ export const googleKeys = {
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 
-  userCallback: "http://localhost:4000/auth/user/google/callback",
-  shopCallback: "http://localhost:4000/auth/shop/google/callback",
+  userCallback:
+    "https://scanmymenu-server.onrender.com/auth/user/google/callback",
+  shopCallback:
+    "https://scanmymenu-server.onrender.com/auth/shop/google/callback",
 };
